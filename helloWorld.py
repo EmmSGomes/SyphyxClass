2bf611826aca18351113b5e1f30c327920b43bcc
EmmSGomes <emmanuel.gomes@live.com>
2015-03-19 21:46:06 -0300

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,7 @@
 turma = ["Andrade", "André", "Daiane", "Dailma", "Diego", "Emmanuel", "Joelson", "Márcio", "Milena", "Rogério", "Ruivo", "Tobias"]
 
+print (turma)
 for estudante in turma:
-  print(estudante)
+    print(estudante)
+
+print "muito doido esse Python"
